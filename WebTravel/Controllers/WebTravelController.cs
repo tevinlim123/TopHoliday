0d2fb1a0f14e5b9ea1a2b3e8e2d9506da4bd848e
tevinlim123 <tevinlim55@gmail.com>
2021-05-06 19:53:29 +0700

    Kelompok 1 (Submit UAS)
    Tevin Lim 2031132
    Micheal 2031136
    Brenky Jie 2031029
    Calvin 2031100
    Jacky 2031135
    Delvin 2031133

---
+++ WebTravel/Controllers/WebTravelController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebTravel.Controllers
+{
+    public class WebTravelController : Controller
+    {
+        // GET: WebTravel
+        public ActionResult Data()
+        {
+            return View();
+        }
+        public ActionResult Data2()
+        {
+            return View();
+        }
+        public ActionResult Medan()
+        {
+            return View();
+        }
+        public ActionResult Yogyakarta()
+        {
+            return View();
+        }
+        public ActionResult Mandalika()
+        {
+            return View();
+        }
+        public ActionResult LabuanBajo()
+        {
+            return View();
+        }
+        public ActionResult Belitung()
+        {
+            return View();
+        }
+        public ActionResult Sumba()
+        {
+            return View();
+        }
+        public ActionResult RajaAmpat()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
